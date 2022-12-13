be339be2d91f87ea364b23090b08073377c2f864
CreatorX8 <RealCreatorX@gmail.com>
2022-12-13 21:31:32 +0200

    create maven project

---
+++ src/main/java/com/creatorx/aslet/AsletApplication.java
@@ -0,0 +1,13 @@
+package com.creatorx.aslet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creatorx/aslet/AsletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creatorx.aslet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
