2ca8c6b1cd7abb9c99122b41d1836198664a2d3f
CreatorX8 <RealCreatorX@gmail.com>
2022-12-17 10:57:08 +0200

    create ClassControllerAdvice.java

---
+++ src/main/java/com/creatorx/aslet/advice/ClassControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.creatorx.aslet.advice;
+
+import com.creatorx.aslet.exception.ClassExistsException;
+import com.creatorx.aslet.exception.ClassNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ClassControllerAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ClassNotFoundException.class)
+    public Map<String, String> handleClassesException(ClassNotFoundException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("status", "404");
+        errorMap.put("error", exception.getMessage());
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    @ExceptionHandler(ClassExistsException.class)
+    public Map<String, String> handleClassesException(ClassExistsException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("status", "406");
+        errorMap.put("error", exception.getMessage());
+        return errorMap;
+    }
+}
