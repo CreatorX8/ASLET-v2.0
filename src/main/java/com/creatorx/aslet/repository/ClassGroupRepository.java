f39a59e2fcaa527c49a4328f08306aab21361d81
CreatorX8 <RealCreatorX@gmail.com>
2022-12-17 12:49:15 +0200

    create ClassGroupRepository.java

---
+++ src/main/java/com/creatorx/aslet/repository/ClassGroupRepository.java
@@ -0,0 +1,7 @@
+package com.creatorx.aslet.repository;
+
+import com.creatorx.aslet.model.ClassGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassGroupRepository extends JpaRepository<ClassGroup, Long> {
+}
