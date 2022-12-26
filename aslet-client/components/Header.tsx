b4d45d268c9d31c6cd3e0e90cd5db22358c8e577
ivokraev <ivo.kraev2@protonmail.com>
2022-12-27 00:25:31 +0200

    Add basic header

---
+++ aslet-client/components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Haeder() {
+    return (
+        <>
+            <header className="bg-gray-400 w-screen">
+                <div className="max-w-5xl mx-auto flex justify-around">
+                    <img src="/" alt="Лого" />
+                    <nav className="flex flex-row space-x-2">
+                        <Link href="/">Какво е Аслет?</Link>
+                        <Link href="">Кои са създателите му?</Link>
+                        <Link href="">Към АСЛЕТ</Link>
+                    </nav>
+                    <div>
+                        <Link href="/auth/login">Влез</Link>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
