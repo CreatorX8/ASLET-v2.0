3b4e35346d32e9cb65043687f8d4c3812ea72078
CreatorX8 <RealCreatorX@gmail.com>
2022-12-28 23:10:08 +0200

    create SubjectRepository.java

---
+++ src/main/java/com/creatorx/aslet/repository/SubjectRepository.java
@@ -0,0 +1,7 @@
+package com.creatorx.aslet.repository;
+
+import com.creatorx.aslet.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+}
