7b2a097067d7e8dc698141287e68c33a44b715b7
CreatorX8 <RealCreatorX@gmail.com>
2022-12-28 12:11:13 +0200

    add validateToken method

--- src/main/java/com/creatorx/aslet/utils/JwtUtils.java
+++ src/main/java/com/creatorx/aslet/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.creatorx.aslet.utils;
 
+import com.creatorx.aslet.exception.NotAuthorizedException;
 import com.creatorx.aslet.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -10,15 +11,14 @@ import java.util.Date;
 
 @Component
 public class JwtUtils {
-//    private final String secret = "NlvGEYYuumyy0OxTGN8D2bxfsICKkXAckIBtccSGTTwnXuhCbSy7zcz3zLKKfpEM3Y1yn0NcwrRcF7rJnN46CpBUzVgll8J4ythL";
-    private final String secret = "ASLET";
+    private final String secret = "NlvGEYYuumyy0OxTGN8D2bxfsICKkXAckIBtccSGTTwnXuhCbSy7zcz3zLKKfpEM3Y1yn0NcwrRcF7rJnN46CpBUzVgll8J4ythL";
 
     public String generateToken(User user) {
         Claims claims = Jwts.claims()
                 .setIssuer(user.getId().toString())
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10));
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)); // 10 hours
 
         claims.put("name", user.getName());
         claims.put("email", user.getEmail());
@@ -27,5 +27,13 @@ public class JwtUtils {
                 .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, secret)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+        } catch (Exception e) {
+            throw new NotAuthorizedException();
+        }
     }
 }
