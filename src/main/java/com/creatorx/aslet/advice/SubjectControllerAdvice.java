27317f5cf30e56efeb631aa154bcbb8d93aa17a2
CreatorX8 <RealCreatorX@gmail.com>
2022-12-28 23:17:01 +0200

    create SubjectControllerAdvice.java

---
+++ src/main/java/com/creatorx/aslet/advice/SubjectControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.creatorx.aslet.advice;
+
+import com.creatorx.aslet.exception.SubjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class SubjectControllerAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(SubjectNotFoundException.class)
+    public Map<String, String> handleSubjectsException(SubjectNotFoundException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("status", "404");
+        errorMap.put("error", exception.getMessage());
+        return errorMap;
+    }
+}
