b06f851c493a43955378a41a02bac0135e5c07e5
CreatorX8 <RealCreatorX@gmail.com>
2022-12-28 20:07:47 +0200

    create TeacherRepository.java

---
+++ src/main/java/com/creatorx/aslet/repository/TeacherRepository.java
@@ -0,0 +1,7 @@
+package com.creatorx.aslet.repository;
+
+import com.creatorx.aslet.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
