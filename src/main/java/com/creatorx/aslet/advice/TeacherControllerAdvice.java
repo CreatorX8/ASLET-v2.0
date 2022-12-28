cefb621d623ad4bf78965b28b38b112ffad52229
CreatorX8 <RealCreatorX@gmail.com>
2022-12-28 23:16:23 +0200

    fix method naming

--- src/main/java/com/creatorx/aslet/advice/TeacherControllerAdvice.java
+++ src/main/java/com/creatorx/aslet/advice/TeacherControllerAdvice.java
@@ -1,6 +1,5 @@
 package com.creatorx.aslet.advice;
 
-import com.creatorx.aslet.exception.ClassGroupNotFoundException;
 import com.creatorx.aslet.exception.TeacherNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,7 +13,7 @@ import java.util.Map;
 public class TeacherControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(TeacherNotFoundException.class)
-    public Map<String, String> handleClassGroupsException(TeacherNotFoundException exception) {
+    public Map<String, String> handleTeachersException(TeacherNotFoundException exception) {
         Map<String, String> errorMap = new HashMap<>();
         errorMap.put("status", "404");
         errorMap.put("error", exception.getMessage());
