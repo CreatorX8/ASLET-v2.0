40eea6bbdf3f5a71f5f11a8ed87eabc9563d6813
CreatorX8 <RealCreatorX@gmail.com>
2022-12-29 13:47:57 +0200

    add findAllByStudentsClass_Owner_Id method

--- src/main/java/com/creatorx/aslet/repository/ClassGroupRepository.java
+++ src/main/java/com/creatorx/aslet/repository/ClassGroupRepository.java
@@ -3,5 +3,8 @@ package com.creatorx.aslet.repository;
 import com.creatorx.aslet.model.ClassGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClassGroupRepository extends JpaRepository<ClassGroup, Long> {
+    public List<ClassGroup> findAllByStudentsClass_Owner_Id(Long id);
 }
