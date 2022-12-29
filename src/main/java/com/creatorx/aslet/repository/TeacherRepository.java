c7b1449cdfcba2a7e95668a6a0af33d2f71b3310
CreatorX8 <RealCreatorX@gmail.com>
2022-12-29 13:57:48 +0200

    add findAllByOwner_Id method

--- src/main/java/com/creatorx/aslet/repository/TeacherRepository.java
+++ src/main/java/com/creatorx/aslet/repository/TeacherRepository.java
@@ -3,5 +3,8 @@ package com.creatorx.aslet.repository;
 import com.creatorx.aslet.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    List<Teacher> findAllByOwner_Id(Long id);
 }
