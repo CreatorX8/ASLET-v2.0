98aaba49d85629932875de7c7e0b9f4d0485cb7f
ivokraev <ivo.kraev2@protonmail.com>
2023-01-07 11:13:55 +0200

    Added custom page for error 404 - page not found

---
+++ aslet-client/pages/404.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+export default function PageNotFound404() {
+    return (
+        <>
+            <Header />
+            <h2>404</h2>
+            <p>Опа, изглежда че такава страница не съществува</p>
+            <p>Помогни си с менюто</p>
+        </>
+    )
+}
